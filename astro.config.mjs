cd999383d380630ea4706eb3177e5486388f2840
VistaValley <vistavalleysea@gmail.com>
2026-04-06 17:15:23 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -54,6 +54,7 @@ export default defineConfig({
                     }
                 }
             ],
+            lastUpdated: true,
             plugins: [
                 starlightSidebarTopics([
                     {
